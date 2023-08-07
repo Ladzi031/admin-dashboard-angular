f6ea9002d52e9040ac617c1ef83b568b9fb230c7
Ladzi <ladziladzani@gmail.com>
2023-08-07 18:48:51 +0200

    overview component added

--- adminDashboard/src/app/app.module.ts
+++ adminDashboard/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
