1e889235c4c5ae1338be9a0c4eab640fa5cc88c9
Ladzi <ladziladzani@gmail.com>
2023-08-07 18:53:50 +0200

    added a pageNotFoundComponent

--- adminDashboard/src/app/app.module.ts
+++ adminDashboard/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    OverviewComponent
+    OverviewComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
