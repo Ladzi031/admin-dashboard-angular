ab6897c8db1cc8ad7f146d331d1e08d9895054ac
Ladzi <ladziladzani@gmail.com>
2023-08-28 14:04:46 +0200

    testing html overview-table with userService

--- adminDashboard/src/app/components/overview/overview.component.ts
+++ adminDashboard/src/app/components/overview/overview.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+
+
+
+type user = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  status: "online" | "offline",
+
+}
+
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
-  constructor(){
-    console.log("hello");
-  }
+export class OverviewComponent implements OnInit {
+
+  public users !: user[]; 
+
+  constructor(private userService: UsersService){}
+ngOnInit(): void {
+  this.users = this.userService.getUser();
+}
 }
--- adminDashboard/src/app/services/users.service.ts
+++ adminDashboard/src/app/services/users.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
 
+type users = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  status: "online" | "offline",
+
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+ 
+public members : users[] =[ 
+  {
+  id: 1,
+  first_name: "james",
+  last_name: "farooq",
+  status: "online"
+},
+{
+  id: 2,
+  first_name: "ladzi",
+  last_name: "ryan",
+  status: "online"
+}, 
+{
+  id: 3,
+  first_name: "myGee",
+  last_name: "kremlin",
+  status: "online"
+},
+{
+  id: 4,
+  first_name: "mosh",
+  last_name: "gosling",
+  status: "online"
+}];
 
   constructor() { }
+
+  getUser(): users[] {
+    return this.members;
+  }
 }
