fde125ec2395341fd268e310bed4b885679e8571
Ladzi <ladziladzani@gmail.com>
2024-01-18 08:26:55 +0200

    add authentication service

--- adminDashboard/src/app/services/auth.service.spec.ts
+++ adminDashboard/src/app/services/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersService);
+    service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
